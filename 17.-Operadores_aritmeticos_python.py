d656f526f5ee6077a4d1a0e8a83b205fbe296d9c
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:26:08 -0600

    Add files via upload

---
+++ 17.-Operadores_aritmeticos_python.py
@@ -0,0 +1,46 @@
+'''
+Este programa muestra los operadores de suma, resta, multiplicación, división, módulo y potencia.
+'''
+numero1 = 20 # Asignamos un valor a la variable numero1 de tipo entero
+numero2 = 5 # Asignamos un valor a la variable numero2 de tipo entero
+multiplicacion1 = numero1 * numero2 # Multiplicamos 2 variables de tipo entero
+print(multiplicacion1) # Imprimimos el valor de suma1
+print(type(multiplicacion1)) # Imprimimos el tipo de variable de suma1
+
+numero3 = 12.5 # Asignamos un valor a la variable de tipo flotante
+numero4 = 9 # Asignamos un valor a la variable de tipo entero: numero4
+resta1 = numero3 - numero4 # Retamos una variable real + variable entero
+print(resta1) # Imprimimos el valor de resta1
+print(type(resta1)) # Imprimimos el tipo de variable de suma2
+
+numero4 = 1.345234 # Le damos otro valor a la variable de tipo real
+numero3 = 6 # Le asignamos a otro valor a la variable de tipo entero
+division1 = numero3 / numero4 # Dividimos el valor de numero3 entre numero4
+print(division1) # Dividimos el valor de suma3 entre suma4
+print(type(division1)) # Imprimimos el tipo de variable de suma3
+
+numero1 = 5 # Asignamos un nuevo valor a la variable de tipo entero
+numero3 = 7657657.1098 # Asignamos un valor a la variable de tipo real
+division2 = numero1 // numero3 # Restamos el valor de numero1 entre numero 3. Me da un entero redondeando hacia abajo.
+print(division2) # Imprimir en pantalla el valor de la suma4
+print(type(division2)) # Imprimimos el tipo de variable de suma4
+
+numero1 = 2 # Le damos otro valor a la variable
+numero2 = 3 # Le damos un nuevo valor a la variable numero2
+potencia = numero1**numero2 # Elevamos 2 al cubo
+print(potencia) # Imprimimos el valor de la variable suma1
+print(type(potencia)) # Imprimimos el tipo de variable de suma1
+
+numero5 = 12 # Asignamos un valor a la variable numero5 de tipo entero
+numero6 = 2 # Asignamos un valor a la variable numero6 de tipo entero
+suma1 = numero5 + numero6 # Sumamos las dos variables de tipo entero
+print(suma1) # Imprimimos el valor de suma1
+print(type(suma1)) # Imprimimos el tipo de variable de suma1
+
+numero7 = 22 # Asignamos un valor a la variable numero7 de tipo entero
+numero8 = 3 # Asignamos un valor a la variable numero8 de tipo entero
+modulo1 = numero7 % numero8 # Obtenemos el residuo de la división entre numero7 y numero8
+print(modulo1) # Imprimimos el valor de modulo1
+print(type(modulo1)) # Imprimimos el tipo de variable de modulo1
+
+# Este programa fue escrito por Emilio Carcaño Bringas
